237df9fece03803b36dcb83b5ca0cb08620fed52
Daniel Zuzevich <danielzuzevich@gmail.com>
2016-07-02 13:31:43 -0400

    Added a function for Name field.
    Created a function called nameCheck that checks to see if the name input field is empty.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,8 @@
 // Javascript goes in here.
+
+function nameCheck() {
+  var x = document.forms['myForm']['name'].value;
+  if(x == null || x == "") {
+    alert("Name must be filled out.");
+    return false;
+  }
