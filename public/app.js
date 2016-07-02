75ec2c2ac678e8fadd0805b4f3abd8af2bd7e87d
Daniel Zuzevich <danielzuzevich@gmail.com>
2016-07-02 14:15:02 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -3,10 +3,9 @@
 function nameCheck() {
   var x = document.forms["myForm"]["name"].value;
   if(x == null || x == "") {
-    var error = document.getElementByClassName("nameError");
-    var y = document.createElement("p")                
-    var z = document.createTextNode("You must enter your name.");    
-    error.y.appendChild(z); 
+    var error = document.getElementByClassName("nameError");            
+    var message = document.createTextNode("You must enter your name.");    
+    error.appendChild(message); 
   }
 };
 
