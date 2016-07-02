950e3fd3a4a8e742afd25ba283195973860bb25b
Daniel Zuzevich <danielzuzevich@gmail.com>
2016-07-02 19:53:56 -0400

    Added an array

--- public/app.js
+++ public/app.js
@@ -1,13 +1,24 @@
 // Javascript goes in here.
 
-function nameCheck() {
-    var x = document.forms["myForm"]["name"].value;
-    if(x == null || x == "") {
-      var error = document.getElementByClassName("nameError");
-      var message = document.createTextNode("You must enter your name.");
-      error.appendChild(message);
+function validate(){
+
+   var validateElements = document.getElementsByClassName("validate");
+
+    var inputs = Array.prototype.filter.call(validateElements, function(element){
+        return element.nodeName === 'INPUT';
+    });
+    /* ^ This Part is a little tricky */
+
+    for(var i=0; i < inputs.length; i ++ ){
+        var input = inputs[i];
+        if(input.value.length == 0){
+            input.placeholder = "This field is required!";
+            input.classList.add("error");
+        }
     }
-  };
+
+}
+
 
 
 
