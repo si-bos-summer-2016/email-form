cb715a357726b8bd19ed258d7f431fdf9634fca1
Daniel Zuzevich <danielzuzevich@gmail.com>
2016-07-02 13:42:38 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 // Javascript goes in here.
 
 function nameCheck() {
-  var x = document.forms['myForm']['name'].value;
+  var x = document.forms["myForm"]["name"].value;
   if(x == null || x == "") {
     alert("Name must be filled out.");
     return false;
