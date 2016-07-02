d8da8f63d5d1fc98d53c49bf2b6d9a21bbbce7ce
Mike Baldwin <mdb255.dev@gmail.com>
2016-07-01 23:57:37 -0400

    Serve static public folder

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT || 8080;
+
+// App
+const app = express();
+
+app.use(express.static('public'));
+
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
