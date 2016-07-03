ae179a15f456d914f5113a11987f5c4ff29f733d
Daniel Zuzevich <danielzuzevich@gmail.com>
2016-07-03 12:18:17 -0400

    Added comments
    Added comments to explain what each piece of code does

--- public/app.js
+++ public/app.js
@@ -3,12 +3,16 @@
 // This creates an array to check if fields are empty
 function validate(){
 
+   // Grabs any HTML element with a class of "validate"
    var validateElements = document.getElementsByClassName("validate");
-
+   
+   // Creates an array of any items from the previous validateElements variable
+   // that have a nodename of "input".
     var inputs = Array.prototype.filter.call(validateElements, function(element){
         return element.nodeName === 'INPUT';
     });
-
+   
+   // Loops through the inputs array to see if any of the fields are empty.
     for(var i=0; i < inputs.length; i ++ ){
         var input = inputs[i];
         if(input.value.length == 0){
