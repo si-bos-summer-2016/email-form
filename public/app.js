bf857431eb776ff975bb6b14f3b5712d86f1f992
Daniel Zuzevich <danielzuzevich@gmail.com>
2016-07-03 10:32:05 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -7,7 +7,6 @@ function validate(){
     var inputs = Array.prototype.filter.call(validateElements, function(element){
         return element.nodeName === 'INPUT';
     });
-    /* ^ This Part is a little tricky */
 
     for(var i=0; i < inputs.length; i ++ ){
         var input = inputs[i];
